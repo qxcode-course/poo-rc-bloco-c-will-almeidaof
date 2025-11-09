6efb5c0b93c9ec6814820a3dc63b0f4b4f7d6f3b
will-almeidaof <willyamabr@gmail.com>
2025-11-09 22:29:08 +0000

    resolvendo essa budega

--- myrep/poo/budega/py/draft.py
+++ myrep/poo/budega/py/draft.py
@@ -5,8 +5,8 @@ class Pessoa:
     def __str__(self):
         return self.nome
     
-class Mercantil:
-    def __init__(self, n_caixas: int): 
+class Budega:
+    def __init__(self, n_caixas):
         self.espera: list[Pessoa] = []
         self.caixas: list[Pessoa | None] = []
         for _ in range(n_caixas):
@@ -15,19 +15,56 @@ class Mercantil:
     def chegar(self, pessoa: Pessoa):
         self.espera.append(pessoa)
 
-    def chamar(self, index:int):
+    def chamar(self, index: int):
         if index < 0 or index >= len(self.caixas):
             print("index invalido")
             return
+            
         if self.caixas[index] is not None:
             print("caixa ocupado")
             return
+    
+        
         if len(self.espera) == 0:
             print("ninguem esperando")
             return
+
         self.caixas[index] = self.espera[0]
         del self.espera[0]
 
-    def finalizar(self, index: int) -> Pessoa | None:
+
+    def finalizar(self, index: int):
+
         if index < 0 or index >= len(self.caixas):
             print("index invalido")
+            return None
+
+        if self.caixas[index] is None:
+            print("caixa ja esta vazio")
+            return None
+        
+        aux = self.caixas[index]
+        self.caixas[index] = None
+        return aux
+
+
+
+
+
+    def __str__(self):
+        caixas = ", ".join([str(x) if x else "----" for x in self.caixas])
+        espera = ", ".join([str(x) for x in self.espera])
+        return f"Caixas: [{caixas}], Espera: [{espera}]"
+        
+
+
+budega = Budega(3)
+print(budega)
+pessoa = Pessoa("Bruno")
+budega.chegar(pessoa)
+print(budega)
+budega.chamar(2)
+print(budega)
+budega.chamar(1)
+budega.finalizar(0)
+print(budega)
