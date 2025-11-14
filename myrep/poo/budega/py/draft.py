7fcaf7099ec92f10e3c7cdebe594899e7606d8e8
will-almeidaof <willyamabr@gmail.com>
2025-11-14 14:23:44 +0000

    resolvendo essa budega part2

--- myrep/poo/budega/py/draft.py
+++ myrep/poo/budega/py/draft.py
@@ -21,12 +21,12 @@ class Budega:
             return
             
         if self.caixas[index] is not None:
-            print("caixa ocupado")
+            print("fail: caixa ocupado")
             return
     
         
         if len(self.espera) == 0:
-            print("ninguem esperando")
+            print("fail: sem clientes")
             return
 
         self.caixas[index] = self.espera[0]
@@ -36,11 +36,11 @@ class Budega:
     def finalizar(self, index: int):
 
         if index < 0 or index >= len(self.caixas):
-            print("index invalido")
+            print("fail: caixa inexistente")
             return None
 
         if self.caixas[index] is None:
-            print("caixa ja esta vazio")
+            print("fail: caixa vazio")
             return None
         
         aux = self.caixas[index]
@@ -52,19 +52,27 @@ class Budega:
 
 
     def __str__(self):
-        caixas = ", ".join([str(x) if x else "----" for x in self.caixas])
+        caixas = ", ".join([str(x) if x else "-----" for x in self.caixas])
         espera = ", ".join([str(x) for x in self.espera])
-        return f"Caixas: [{caixas}], Espera: [{espera}]"
-        
+        return f"Caixas: [{caixas}]\nEspera: [{espera}]"
 
 
-budega = Budega(3)
-print(budega)
-pessoa = Pessoa("Bruno")
-budega.chegar(pessoa)
-print(budega)
-budega.chamar(2)
-print(budega)
-budega.chamar(1)
-budega.finalizar(0)
-print(budega)
+def main():
+    budega = Budega(0)
+    while True:
+        line = input()
+        print("$"+line)
+        args: list[str] = line.split()
+        if args[0] == "end":
+            break
+        elif args[0] == "init":
+            budega = Budega(int(args[1]))
+        elif args[0] == "show":
+            print(budega)
+        elif args[0] == "arrive":
+            budega.chegar(args[1])
+        elif args[0] == "call":
+            budega.chamar(int(args[1]))
+        elif args[0] == "finish":
+            budega.finalizar(int(args[1]))
+main()
