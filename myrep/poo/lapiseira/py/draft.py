39c10eff35e0dec87b728dd9c7aa5aa90b1823cb
will-almeidaof <willyamabr@gmail.com>
2025-11-19 20:27:14 +0000

    terminei da lapiseira

--- myrep/poo/lapiseira/py/draft.py
+++ myrep/poo/lapiseira/py/draft.py
@@ -1,28 +1,43 @@
 class Grafite:
-    def __init__(self, dureza : str, calibre: float, size: int):
+    def __init__(self,calibre: float, dureza : str, size: int):
         self.dureza = dureza
         self.calibre = calibre
         self.tamanho = size
 
     def get_calibre(self):
         return self.calibre
+    def get_tamanho(self):
+        return self.tamanho
+    def set_tamanho(self, tamanho: int):
+        self.tamanho = tamanho
 
-
+    def GastoFolha(self):
+        if self.dureza == "HB":
+            return 1
+        elif self.dureza == "2B":
+            return 2
+        elif self.dureza == "4B":
+            return 4
+        elif self.dureza == "6B":
+            return 6
+        else:
+            return 0
 
     def __str__(self):
-        return f"{self.dureza}:{self.calibre}:{self.tamanho}"
+        return f"[{self.calibre}:{self.dureza}:{self.tamanho}]"
     
 
 class Lapiseira:
     def __init__(self,calibre : float):
         self.calibre = calibre
-        self.bico: list[Grafite|None] = [None]
+        self.bico: Grafite|None = None
         self.tambor: list[Grafite] = []
 
-    def inserir(self, grafite: Grafite):
-        if self.calibre != grafite.get_calibre():
+    def inserir(self,calibre: float, dureza: str, tamanho: int):
+        if calibre != self.calibre:
             print("fail: calibre incompatível")
             return
+        grafite = Grafite(calibre, dureza,tamanho)
         self.tambor.append(grafite)
 
 
@@ -30,30 +45,71 @@ class Lapiseira:
     def puxar(self):
         if self.bico is not None:
             print("fail: ja existe grafite no bico")
-            return
-        
-        if len(self.tambor) == 0:
-            print("fail: tambor esta vazio")
-            return
+            return False
+        elif not self.tambor:
+            print("fail: sem grafite")
+            return False
 
-        self.bico[0] = self.tambor[0]
+        self.bico = self.tambor[0]
         del self.tambor[0]
+        return True
+
+    def escreverPag(self):
+        if self.bico is None:
+            print("fail: nao existe grafite no bico")
+            return
+        if self.bico.get_tamanho() <= 10:
+            print("fail: tamanho insuficiente")
+            self.bico = None
+            return
+        consumo = self.bico.GastoFolha()
+        if self.bico.get_tamanho() - consumo < 10:
+            print("fail: folha incompleta")
+            self.bico.set_tamanho(10)
+            return
+        self.bico.set_tamanho(self.bico.get_tamanho()-consumo)
+
+
+    def remover(self):
+        if self.bico is None:
+            print("fail: não ha grafite no bico ")
+            return 
+        aux = self.bico
+        self.bico = None
+        return aux
 
 
     def __str__(self):
-        tambor = ":".join([str(x)if x else "" for x in self.tambor])
-        bico = ":".join([str(x)if x else "" for x in self.bico])
-        return f"calibre: {self.calibre}, bico: [{bico}], tambor: <{tambor}>"
+        tambor = "".join([str(x)if x else "" for x in self.tambor])
+        bico = f"{self.bico}" if self.bico else "[]"
+        return f"calibre: {self.calibre}, bico: {bico}, tambor: <{tambor}>"
 
 
-lapiseira = Lapiseira(0.5)
-grafite = Grafite("2B",0.5,10)
-lapiseira.inserir(grafite)
-print(lapiseira)
-lapiseira.puxar()
-print(lapiseira)
-grafite1 = Grafite("2B",0.5,10)
-lapiseira.inserir(grafite1)
-print(lapiseira)
-lapiseira.puxar()
-print(lapiseira)
+def main():
+    lapiseira = Lapiseira(0)
+    while True:
+        line = input()
+        print("$"+line)
+        args = line.split()
+        if args[0] == "end":
+            break
+        elif args[0] == "show":
+            print(lapiseira)
+        elif args[0] == "init":
+            calibre = float(args[1])
+            lapiseira = Lapiseira(calibre)
+        elif args[0] == "insert":
+            calibre = float(args[1])
+            dureza = args[2]
+            tamanho = int(args[3])
+            lapiseira.inserir(calibre, dureza, tamanho)
+        elif args[0] == "pull":
+            lapiseira.puxar()
+        elif args[0] == "remove":
+            lapiseira.remover()
+        elif args[0] == "write":
+            lapiseira.escreverPag()
+        
+
+
+main()
